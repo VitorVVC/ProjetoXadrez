de6e523c9bcdac0e8547e878e336f590617bd38d
Vitor <vitorvvc2005@gmail.com>
2024-01-02 13:00:35 -0300

    Handling exceptions and clearing screen

--- src/Model/Application/Main.java
+++ src/Model/Application/Main.java
@@ -1,9 +1,11 @@
 package Model.Application;
 
+import Model.Chess.ChessException;
 import Model.Chess.ChessMatch;
 import Model.Chess.ChessPiece;
 import Model.Chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -11,15 +13,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         while (true) { // While apenas para testar a funcão de movimento das peç9as
-            UI.printBoard(chessMatch.getPieces()); // Método de UI ( User Interface ). Que serve para printar o board criado fora do While
-            System.out.println();
-            System.out.print("Source: "); // Pedimos uma "coordenada" de qual a peça
-            ChessPosition source = UI.readChessPosition(sc); // Recebemos e armazenamos a mesma na função de ler estas coordenadas dadas em "formato xadrez"
-            System.out.print("Target: "); // Pedimos uma "coordenada" de destino
-            ChessPosition target = UI.readChessPosition(sc); // Recebemos e armazenamos tal coordenada na função antes usada
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source,target); // Fazemos um movimento passando a coordenada de entrada e o destino com a função "performChessMove".
+            try {
+                UI.clearScreen();
 
+                UI.printBoard(chessMatch.getPieces()); // Método de UI ( User Interface ). Que serve para printar o board criado fora do While
+                System.out.println();
+                System.out.print("Source: "); // Pedimos uma "coordenada" de qual a peça
+                ChessPosition source = UI.readChessPosition(sc); // Recebemos e armazenamos a mesma na função de ler estas coordenadas dadas em "formato xadrez"
+                System.out.print("Target: "); // Pedimos uma "coordenada" de destino
+                ChessPosition target = UI.readChessPosition(sc); // Recebemos e armazenamos tal coordenada na função antes usada
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target); // Fazemos um movimento passando a coordenada de entrada e o destino com a função "performChessMove".
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
 
 
--- src/Model/Application/UI.java
+++ src/Model/Application/UI.java
@@ -30,6 +30,13 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    // Código para limpar as telas
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+
     public static void printBoard(ChessPiece[][] pieces) { // Considerando matriz quadrada
         int pieceLenght = pieces.length;
         for (int i = 0; i < pieceLenght; i++) {
